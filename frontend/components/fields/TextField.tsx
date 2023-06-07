664a1dde39912973b3681ad19c9c41a70b775b97
Aryan Jain <aryanjn807@gmail.com>
2023-06-07 11:39:45 -0400

    -> added verification modal

--- frontend/components/fields/TextField.tsx
+++ frontend/components/fields/TextField.tsx
@@ -1,17 +1,25 @@
-import { TextField } from "@mui/material";
+import {
+  FilledInputProps,
+  InputProps,
+  OutlinedInputProps,
+  TextField,
+  TextFieldVariants,
+} from "@mui/material";
 
 const TextFieldComp = (props: TextFieldProps) => {
   const {
+    variant,
     sx = {},
     type = "text",
     value,
     onChange,
     label,
     required = false,
-    endIcon,
+    InputProps = {},
   } = props;
   return (
     <TextField
+      variant={variant}
       sx={{ m: 1.5, ...sx }}
       type={type}
       label={label}
@@ -19,19 +27,23 @@ const TextFieldComp = (props: TextFieldProps) => {
       onChange={({ target: { value } }) => onChange(value)}
       required={required}
       fullWidth
-      InputProps={{ endAdornment: endIcon }}
+      InputProps={InputProps}
     />
   );
 };
 
 interface TextFieldProps {
+  variant?: TextFieldVariants; // "contained" | "outlined" | "filled";
   sx?: any;
   value: string | null;
   onChange: (p: string) => void;
   label: string;
   required?: boolean;
   type?: string;
-  endIcon?: React.ReactNode;
+  InputProps?:
+    | Partial<FilledInputProps>
+    | Partial<OutlinedInputProps>
+    | Partial<InputProps>;
 }
 
 export default TextFieldComp;
--- frontend/components/forms/Register.tsx
+++ frontend/components/forms/Register.tsx
@@ -17,7 +17,7 @@ import Link from "next/link";
 import TextField from "../fields/TextField";
 import PasswordField from "../fields/PasswordField";
 import { useRegisterUser } from "../../hooks/user.hooks";
-import { CloseOutlined } from "@mui/icons-material";
+import { CloseOutlined, Visibility } from "@mui/icons-material";
 
 export const RegisterForm = () => {
   const { mutate: registerUser, isLoading } = useRegisterUser();
@@ -40,6 +40,19 @@ export const RegisterForm = () => {
   };
 
   const [fields, setFields] = useState<RegisterFields>(initFields);
+  const [code, setCode] = useState<number | null>(null);
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleVerificationCode = (code: string) => {
+    if (code) {
+      if (code.length <= 6) {
+        setCode(Number(code));
+      }
+    } else {
+      setCode(null);
+    }
+  };
+
   return (
     <>
       <Box
@@ -76,15 +89,25 @@ export const RegisterForm = () => {
           value={fields.email}
           onChange={(email) => setFields({ ...fields, email })}
           required
-          endIcon={
-            <Box display="flex" alignContent="center" justifyContent="end">
-              <div onClick={() => {}}>
-                <Typography sx={{ cursor: "pointer" }} variant="body2">
-                  Verify
-                </Typography>
-              </div>
-            </Box>
-          }
+          InputProps={{
+            endAdornment: fields?.email ? (
+              <Box display="flex" alignContent="center" justifyContent="end">
+                <div onClick={() => setOpen(true)}>
+                  <Typography
+                    sx={{
+                      color: "green",
+                      cursor: "pointer",
+                    }}
+                    variant="body2"
+                  >
+                    Verify
+                  </Typography>
+                </div>
+              </Box>
+            ) : (
+              <></>
+            ),
+          }}
         />
         <PasswordField
           value={fields.password}
@@ -115,7 +138,7 @@ export const RegisterForm = () => {
         </Box>
       </Box>
 
-      <Dialog open={true} fullWidth maxWidth="sm">
+      <Dialog open={open} fullWidth maxWidth="sm">
         <DialogTitle>
           <Box
             display="flex"
@@ -123,7 +146,14 @@ export const RegisterForm = () => {
             justifyContent="space-between"
           >
             <Typography>Modal Title</Typography>
-            <CloseOutlined />
+            <div
+              onClick={() => {
+                setOpen(false);
+                setCode(null);
+              }}
+            >
+              <CloseOutlined sx={{ cursor: "pointer" }} />
+            </div>
           </Box>
         </DialogTitle>
         <Divider />
@@ -140,18 +170,20 @@ export const RegisterForm = () => {
               Enter 6-digit verification code sent to "{fields.email}"
             </Typography>
             <TextField
+              variant="filled"
               type="number"
-              value=""
-              onChange={() => {}}
+              value={String(code)}
+              onChange={handleVerificationCode}
               label="Verification Code"
-              sx={{ mx: 0, my: 2, width: "40%" }}
+              sx={{ mx: 0, my: 2, width: "50%", spacing: 212 }}
+              InputProps={{ style: { fontSize: 30, letterSpacing: 18 } }}
             />
           </Box>
         </DialogContent>
 
         <Divider />
         <DialogActions>
-          <Button variant="contained" disabled>
+          <Button variant="contained" disabled={String(code).length !== 6}>
             Verify
           </Button>
         </DialogActions>
